e6eaeebf32bfeab79ef006dbc8fea65c502fac79
NikolayRag <nikolayr@mail.ru>
2020-04-14 05:29:19 +0300

    start

---
+++ web/bpm.js
@@ -0,0 +1,12 @@
+//  todo 2 (feature) +0: Detect beat from mic
+//  todo 3 (feature) +0: Show instant beat pulse, BpM value
+//  todo 4 (feature) +0: Show mean BpM, predicted pulse, delta error in % and time
+//  todo 5 (feature) +0: Show relatively constant BpM blocks over time
+//  todo 6 (feature) +0: Explicit or implicit metronome, light/audio
+//  todo 7 (feature) +0: Detect major and minor beats
+
+//  todo 8 (feature) +0: Make sociable!
+//  todo 9 (feature) +0: Score of stability
+//  todo 10 (feature) +0: Score of artistism
+//  todo 11 (feature) +0: Progress over sessions
+//  todo 12 (feature) +0: Store best scored beat images
